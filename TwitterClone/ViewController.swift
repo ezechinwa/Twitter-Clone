16f9fd3c87e1556b1cf118381710be5d20e6bea4
Chinwa Williams <chinwawilliams@gmail.com>
2019-02-24 01:01:46 +0100

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Chinwa Williams on 24/02/2019.
+//  Copyright © 2019 Chinwa Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
